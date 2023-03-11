4a3755ac39d25590bf9ea8af439a781bb1fac192
Augusto <augustoluna64@gmail.com>
2023-03-11 12:30:50 -0300

    feat: adicionado divisão

--- calculadora.js
+++ calculadora.js
@@ -6,6 +6,14 @@ const sub = () => {
     console.log(parseInt(args[1]) - parseInt(args[2]));  
 }
 
+const div = () =>{
+    if (args[2] == 0){
+        console.log('Não é possível dividir!');
+    }
+    else
+    console.log(parseInt(args[1])/parseInt(args[2]));
+}
+
 const args = process.argv.slice(2);
 
 switch (args[0]) {
@@ -15,6 +23,9 @@ switch (args[0]) {
 
     case 'sub':
         sub();
+
+    case 'div':
+        div();
     break;
 
     default:
